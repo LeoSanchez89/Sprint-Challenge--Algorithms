c36619ab4a82033699f9d92477f712318f701429
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-19 17:34:05 -0400

    robot test passing

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,16 +100,28 @@ class SortingRobot:
         # pickup first item
         # move right
         # compare held item to next item
-            # if next item is smaller, swap until cant move right anymore
-        # move left until cant move anymore
-       
-        # move left while prev item is larger than held item
-        # if prev item is smaller than held item, move right 1 and swap
+        # if next item is smaller, swap until cant move right anymore
+        # move left until reaching none 
+        # swap num with none, move right and swap again
         # restart moving right and repeat
         
-
-        print(self._item)
-        pass
+        self.swap_item()
+        self.set_light_on()
+        while self.light_is_on():
+            while self.can_move_right() == True:
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+            while self.compare_item() is not None:
+                self.move_left()
+            self.swap_item()
+            if self.can_move_right() == False:
+                self.set_light_off()
+            else:
+                self.move_right()
+                self.swap_item()
+                    
+            
 
 
 if __name__ == "__main__":
@@ -117,7 +129,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-        0
+
     robot = SortingRobot(l)
 
     robot.sort()
