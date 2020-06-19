8cb1b8c414f2bb34c0a789a48b9c572f4734f82f
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-19 14:15:14 -0400

    robot sort planning but not complete

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,6 +97,18 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
+        # pickup first item
+        # move right
+        # compare held item to next item
+            # if next item is smaller, swap until cant move right anymore
+        # move left until cant move anymore
+       
+        # move left while prev item is larger than held item
+        # if prev item is smaller than held item, move right 1 and swap
+        # restart moving right and repeat
+        
+
+        print(self._item)
         pass
 
 
@@ -105,7 +117,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+        0
     robot = SortingRobot(l)
 
     robot.sort()
